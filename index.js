31388422e8ab4f3f02636d0003e466f188e1a016
Sailesh Dhungana <dhunganasailesh@gmail.com>
2020-03-02 10:19:04 +0545

    Trying to make it work

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // @flow
 
-import PdfHighlighter from "./lib/components/PdfHighlighter";
-import Tip from "./lib/components/Tip";
-import Highlight from "./lib/components/Highlight";
-import Popup from "./lib/components/Popup";
-import AreaHighlight from "./lib/components/AreaHighlight";
-import PdfLoader from "./lib/components/PdfLoader";
+import PdfHighlighter from "./src/components/PdfHighlighter";
+import Tip from "./src/components/Tip";
+import Highlight from "./src/components/Highlight";
+import Popup from "./src/components/Popup";
+import AreaHighlight from "./src/components/AreaHighlight";
+import PdfLoader from "./src/components/PdfLoader";
 
 export { PdfHighlighter, Tip, Highlight, Popup, AreaHighlight, PdfLoader };
