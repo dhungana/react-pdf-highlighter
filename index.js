4a391db5717489925b9dee9a1f41f44232130a5c
Sailesh Dhungana <dhunganasailesh@gmail.com>
2020-03-02 10:50:58 +0545

    Trying to make this into a usable library

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // @flow
 
-import PdfHighlighter from "./src/components/PdfHighlighter";
-import Tip from "./src/components/Tip";
-import Highlight from "./src/components/Highlight";
-import Popup from "./src/components/Popup";
-import AreaHighlight from "./src/components/AreaHighlight";
-import PdfLoader from "./src/components/PdfLoader";
+import PdfHighlighter from "./build/components/PdfHighlighter";
+import Tip from "./build/components/Tip";
+import Highlight from "./build/components/Highlight";
+import Popup from "./build/components/Popup";
+import AreaHighlight from "./build/components/AreaHighlight";
+import PdfLoader from "./build/components/PdfLoader";
 
 export { PdfHighlighter, Tip, Highlight, Popup, AreaHighlight, PdfLoader };
