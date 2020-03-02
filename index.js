0278c11329869fd918f8b0b7c7e35900a131c837
Sailesh Dhungana <dhunganasailesh@gmail.com>
2020-03-02 10:10:51 +0545

    Moved index.js to make library work

---
+++ index.js
@@ -0,0 +1,10 @@
+// @flow
+
+import PdfHighlighter from "./lib/components/PdfHighlighter";
+import Tip from "./lib/components/Tip";
+import Highlight from "./lib/components/Highlight";
+import Popup from "./lib/components/Popup";
+import AreaHighlight from "./lib/components/AreaHighlight";
+import PdfLoader from "./lib/components/PdfLoader";
+
+export { PdfHighlighter, Tip, Highlight, Popup, AreaHighlight, PdfLoader };
